d8a48976e343650d8be3eb7dcfc5ae116863a921
nitin.mewada1992 <nitin.mewada1992@gmail.com>
2021-06-01 17:22:27 +0530

    parameter pass to template

--- mysite/views.py
+++ mysite/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 def index(request):
     # return HttpResponse("<center>This is Home Page..<br><a href='about'>About</a> <a href='services'>Services</a>  <a href='contact'>Contact</a></center>")
-    return render(request,'index.html')
+    # return render(request,'index.html')
+    data={'name':'Nitin','place':'Mumbai'}
+    return render(request,'index.html',data)
 
 def about(request):
     return HttpResponse("This is About us Page..<a href='/'>Back</a>")
